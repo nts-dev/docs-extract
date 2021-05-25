602542f2a8a9d37f3dda3ca4c7ad9806327d9311
Geoffrey <Geoffrey@nts.nl>
2021-05-25 14:39:51 +0300

    update

--- index.php
+++ index.php
@@ -85,11 +85,11 @@ include('../includes.php');
         }
 
         .dhxlayout_base_dhx_terrace div.dhx_cell_layout div.dhx_cell_ribbon_def div.dhtmlxribbon_dhx_terrace.dhxrb_without_tabbar {
-            height: 100px;
+            height: 120px;
         }
 
         .dhtmlxribbon_dhx_terrace .dhxrb_block_base {
-            height: 90px;
+            height: 110px;
             width: auto;
         }
     </style>
